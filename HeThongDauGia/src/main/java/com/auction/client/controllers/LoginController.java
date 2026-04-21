b9f092ab6f5873d67066e72236ff53bf117e654b
Trần Thành Trung <25022035@vnu.edu.vn>
2026-04-22 02:17:07 +0700

    login screen rebuild

--- HeThongDauGia/src/main/java/com/auction/client/controllers/LoginController.java
+++ HeThongDauGia/src/main/java/com/auction/client/controllers/LoginController.java
@@ -7,25 +7,20 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
 
-    // Khai báo các biến khớp đúng y hệt với fx:id bên file FXML
     @FXML
     private TextField txtUsername;
 
     @FXML
     private PasswordField txtPassword;
 
-    // Hàm này sẽ chạy khi ông bấm cái nút có onAction="#handleLogin"
     @FXML
     public void handleLogin(ActionEvent event) {
-        // Lấy chữ người dùng nhập
         String user = txtUsername.getText();
         String pass = txtPassword.getText();
 
-        // In tạm ra console để test xem có ăn dữ liệu chưa
         System.out.println("Tài khoản: " + user);
         System.out.println("Mật khẩu: " + pass);
 
-        // Sau này có Backend thì mình sẽ gọi API kiểm tra đăng nhập ở đoạn này
         if (user.isEmpty() || pass.isEmpty()) {
             System.out.println("Lỗi: Không được để trống!");
         } else {
