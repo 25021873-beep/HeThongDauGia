ec21af253a54dfdd99e764ac51002cc7892e9b01
25021699 <25021699@vnu.edu.vn>
2026-05-04 08:39:07 +0700

    sửa ClientHandler

--- HeThongDauGia/src/main/java/org/example/network/ClientHandler.java
+++ HeThongDauGia/src/main/java/org/example/network/ClientHandler.java
@@ -9,11 +9,15 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
-
+//Đường dây nối mạng trực tiếp đến khách
     private final Socket clientSocket;
+//Oong nghe phản hồi của khách
     private BufferedReader in;
+ //Phản hồi lại
     private PrintWriter out;
+ //Sổ tay lưu truwx xem mình đang phục vụ ai
     private String clientId;
+//Kiểm tra xem khách ứng ở phòng naof
     private String watchingAuctionId;
 
     public ClientHandler(Socket clientSocket) {
@@ -24,14 +28,17 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
+
             in  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
 
             System.out.println("Handler khởi động cho: " + clientSocket.getInetAddress());
 
             String raw;
+            //Nhânvienen ngồi canh ống nghe liên tucj
             while ((raw = in.readLine()) != null) {
                 try {
+                    //Nếu có khách liên lạc bằng chuỗi raw sẽ chuyển chuỗi raw thành nội dung đọc được,rồi ửi cho hàm xử lý tinh nắn
                     Message msg = Message.fromJson(raw);
                     handleMessage(msg);
                 } catch (Exception e) {
@@ -43,14 +50,14 @@ public class ClientHandler implements Runnable {
         } catch (IOException e) {
             System.out.println("Cilent ngắt kết nối: " + clientId);
         } finally {
-            AuctionServer.removeCilent(this);
+            AuctionServer.removeClient(this);
             closeConnection();
         }
     }
 
     private void handleMessage(Message msg) {
         System.out.println("[" + msg.getType() + "] từ " + clientId + " | " + msg.getPayload());
-
+//Nhận biết dựa trên phong bì thu mà phân loại nhung nhiệm vụ cân làm
         switch (msg.getType()) {
             case Message.LOGIN:
                 handleLogin(msg);
@@ -65,7 +72,7 @@ public class ClientHandler implements Runnable {
                 sendMessage(new Message(Message.ERROR, "Loại tin nhắn không xác định", "SERVER"));
         }
     }
-
+//Bóc chuỗi lấy tn người dùng
     private void handleLogin(Message msg) {
         // TODO: Backend thêm UserService.authenticate() vào đây
         String username = extractField(msg.getPayload(), "username");
@@ -79,7 +86,7 @@ public class ClientHandler implements Runnable {
         System.out.println("Đăng nhập thành công: " + clientId);
         sendMessage(new Message(Message.LOGIN_OK, "Xin chào " + clientId, "SERVER"));
     }
-
+//Tìm phòng mà khách muốn vào
     private void handleJoinAuction(Message msg) {
         String auctionId = msg.getPayload();
 
@@ -92,7 +99,7 @@ public class ClientHandler implements Runnable {
         System.out.println(clientId + " vào xem phiên: " + auctionId);
         sendMessage(new Message(Message.JOIN_AUCTION, "Đã vào phòng: " + auctionId, "SERVER"));
     }
-
+//Kiểm tra xem khách đặt giá có đúng phòng không
     private void handlePlaceBid(Message msg) {
         // TODO: Backend thêm BidService.placeBid() vào đây
         String auctionId = extractField(msg.getPayload(), "auctionId");
@@ -110,13 +117,13 @@ public class ClientHandler implements Runnable {
         System.out.println(clientId + " đặt giá trong phiên: " + auctionId);
         AuctionServer.broadcastToAuction(auctionId, new Message(Message.BID_UPDATE, msg.getPayload(), clientId));
     }
-
+//Chỉ cần cho msg vào đây, sẽ tự động chuyển thành chuỗi raw
     public void sendMessage(Message msg) {
         if (out != null) {
             out.println(msg.toJson());
         }
     }
-
+//Đóng kết nối
     private void closeConnection() {
         try {
             if (in != null)           in.close();
@@ -127,6 +134,7 @@ public class ClientHandler implements Runnable {
         }
     }
 
+//Cắt chuỗi tìm đúng giá trị cần tìm
     private String extractField(String json, String fieldName) {
         if (json == null) return null;
         String key = "\"" + fieldName + "\":\"";
