392053b772ed40a800254d585ac57e4b2012fe8c
Trần Thành Trung <25022035@vnu.edu.vn>
2026-05-29 11:49:34 +0700

    fix bug logic

--- HeThongDauGia/src/main/java/org/example/service/AuctionService.java
+++ HeThongDauGia/src/main/java/org/example/service/AuctionService.java
@@ -89,14 +89,16 @@ public class AuctionService {
         newAuction.setItemId(itemId);
         newAuction.setSellerId(sellerId);
         newAuction.setStartingPrice(startingPrice);
+        newAuction.setStepPrice(stepPrice);
         newAuction.setStartTime(startTime);
         newAuction.setEndTime(endTime);
-        newAuction.setSellerId(item.getSellerId());
-        if (LocalDateTime.now().isAfter(startTime)) throw new InvalidAuctionTimeException("Lỗi: Thời gian bắt đầu phải sau thời điểm hiện tại");
+        newAuction.setCurrentPrice(startingPrice);
+
+        // Nếu startTime đã qua hoặc cách now ≤ 2 phút → bắt đầu ngay (RUNNING)
+        // Ngược lại → trạng thái chờ (OPEN)
         if (!startTime.isAfter(LocalDateTime.now())) {
             newAuction.setStatus("RUNNING");
         } else {
-            // Nếu startTime > hiện tại (tương lai xa) -> ĐỂ TRẠNG THÁI CHỜ
             newAuction.setStatus("OPEN");
         }
 
